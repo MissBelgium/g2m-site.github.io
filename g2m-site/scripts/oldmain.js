ecf80f4d8a0b90c400ac7e1a3760dc5fdc3271b2
MissBelgium <56732722+MissBelgium@users.noreply.github.com>
2022-12-01 17:29:29 -0500

    test website grindin2maintain

---
+++ g2m-site/scripts/oldmain.js
@@ -0,0 +1,15 @@
+const myHeading = document.querySelector("h1");
+myHeading.textContent = "Hello world!";
+
+let iceCream = "chocolate";
+if (iceCream !== "chocolate") {
+  alert("Yay, I love chocolate ice cream!");
+} else {
+  alert("Awwww, but chocolate is my favorite…");
+}
+
+function multiply(num1, num2) {
+  let result = num1 * num2;
+  return result;
+}
+
